25ac06c6a4490d303583428c72f58119bef466c2
tim-hennessey <tim.hennessey@deutschinc.com>
2018-07-07 13:59:57 -0700

    creative update

---
+++ app/160x600/scripts/Animation.js
@@ -0,0 +1,54 @@
+var app = app || {};
+
+app.Animation = (function () {
+
+    var t = TweenMax;
+    var tl1 = new TimelineMax();
+    var txt1 = document.getElementById('txt1');
+    var bag1 = document.getElementById('bag1');
+    var bag2 = document.getElementById('bag2');
+    var bag3 = document.getElementById('bag3');
+
+    var cta = document.getElementById('cta');
+    var cta_txt = document.getElementById('cta_txt');
+    var cta_txt_ovr = document.getElementById('cta_txt_ovr');
+    var buttonExit = document.getElementById('button-exit');
+
+	// --------------------------------------------------------------------------------------
+	function initialize() {
+		t.set(banner, {opacity:1});
+
+        buttonExit.addEventListener('mouseover', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#ffffff"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:1});
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#cc0000"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:0});
+        });
+
+	}
+	// --------------------------------------------------------------------------------------
+	function start() {
+        tl1.from(bag1, 1, {x:100, ease: Cubic.easeOut})
+            .from(bag2, 1, {x:-100, ease: Cubic.easeOut},"-=.75")
+            .from(bag3, 1, {x:100, ease: Cubic.easeOut},"-=.8")
+            .from(txt1, 1, {y: -84, ease: Cubic.easeInOut})
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
---
+++ app/300x250/scripts/Animation.js
@@ -0,0 +1,52 @@
+var app = app || {};
+
+app.Animation = (function () {
+
+    var t = TweenMax;
+    var tl1 = new TimelineMax();
+    var txt1 = document.getElementById('txt1');
+    var img1 = document.getElementById('img1');
+
+
+    var cta = document.getElementById('cta');
+    var cta_txt = document.getElementById('cta_txt');
+    var cta_txt_ovr = document.getElementById('cta_txt_ovr');
+    var buttonExit = document.getElementById('button-exit');
+
+	// --------------------------------------------------------------------------------------
+	function initialize() {
+        // t.set(img1, {transformOrign:'top right'});
+        t.set(banner, {opacity:1});
+
+        buttonExit.addEventListener('mouseover', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#ffffff"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:1});
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            TweenMax.to(cta, .25, {backgroundColor: "#cc0000"});
+            TweenMax.to(cta_txt_ovr, .25, {opacity:0});
+        });
+
+	}
+	// --------------------------------------------------------------------------------------
+	function start() {
+        tl1.from(img1, 2, {scale:1.1, transformOrigin:"right top", ease: Cubic.easeOut})
+            .from(txt1, 1, {y: -84, ease: Cubic.easeInOut});
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
